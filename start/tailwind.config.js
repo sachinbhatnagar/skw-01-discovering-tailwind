1bcf3b8f9e1e5c800804ead7e3f240f3305d7885
Sachin Bhatnagar <mailme@sachinbhatnagar.com>
2022-02-11 14:23:29 +0530

    Refresh v1.11

--- start/tailwind.config.js
+++ start/tailwind.config.js
@@ -1,20 +1,7 @@
 module.exports = {
   content: ['./src/index.html', './src/css/*'],
-  darkMode: 'class',
   theme: {
-    extend: {
-      boxShadow: {
-        premium: '0px 0px 40px 5px rgba(89, 0, 255, 0.7)',
-      },
-      colors: {
-        purple: {
-          950: 'rgb(109, 13, 210)',
-        },
-      },
-      fontFamily: {
-        sans: ["'Open Sans'"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
